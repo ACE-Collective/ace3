5d05cc21c1335191d52dee983e729e4d987be1cc
John Davison <unixfreak0037@gmail.com>
2025-10-01 18:49:23 +0000

    initialize ace env for integration installation scripts

--- bin/install_integrations.sh
+++ bin/install_integrations.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+cd /opt/ace
+source /venv/bin/activate
+source load_environment
+
 cd /opt/ace && \
     find integrations -type d -maxdepth 1 -mindepth 1 | while read dir
     do
