ce2922dbb4c0021e356c4dd7f8caac185ff99ed3
John Davison <unixfreak0037@gmail.com>
2026-02-16 18:31:11 +0000

    handle phishkit volume init correctly

--- docker/startup/initialize_volumes.sh
+++ docker/startup/initialize_volumes.sh
@@ -2,11 +2,27 @@
 #
 
 #
+# special handling for the phishkit volume
+#
+
+if [ ! -d /phishkit/input ]
+then
+    mkdir -p /phishkit/input
+    chown ace:ace /phishkit/input
+fi
+
+if [ ! -d /phishkit/output ]
+then
+    mkdir -p /phishkit/output
+    chown ace:ace /phishkit/output
+fi
+
+#
 # when docker creates a named volume it creates it owned root:root
 # this ensures that the volumes are owned by ace instead
 #
 
-for path in /opt/ace/data /opt/ace/data/external /opt/ace/ssl /docker-entrypoint-initdb.d /ace-sql-readonly /auth /home/ace /phishkit
+for path in /opt/ace/data /opt/ace/data/external /opt/ace/ssl /docker-entrypoint-initdb.d /ace-sql-readonly /auth /home/ace /phishkit /phishkit/input /phishkit/output
 do
     if [ -d "${path}" ]
     then
