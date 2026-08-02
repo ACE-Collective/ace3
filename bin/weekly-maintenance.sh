6acb087756883c3184cc44aa1b54e9a7338aa0d5
John Davison <unixfreak0037@gmail.com>
2026-08-02 13:07:16 +0000

    removed unused script and added cache management to weekly cron job

--- bin/weekly-maintenance.sh
+++ bin/weekly-maintenance.sh
@@ -3,3 +3,6 @@ source /opt/ace/bin/initialize-environment.sh
 
 # maintain email archive database partitions
 bin/manage-email-archive-partitions.sh
+
+# maintain analysis result cache database partitions
+bin/manage-analysis-result-cache-partitions.sh
